d8c1d5c9d5200e6a9eb1262e9f575eaf06ec8972
Gábor Purgel <gaborpurgel0@gmail.com>
2024-02-11 22:58:16 +0100

    Typescript Functions 3

--- feladat1.js
+++ feladat1.js
@@ -12,5 +12,47 @@ function korKerTer(r) {
     var terulet = r * r * Math.PI;
     return [kerulet, terulet];
 }
-console.log(negativE([1, 0, 3, 6, -8]));
-console.log(korKerTer(6));
+function minAuto(auto) {
+    var minCCM = auto[0];
+    for (var i = 0; i < auto.length; i++) {
+        if (minCCM > auto[i]) {
+            minCCM = auto[i];
+        }
+    }
+    return minCCM;
+}
+function benzinesDB(auto) {
+    var benzinesDarabSzam = 0;
+    for (var i = 0; i < auto.length; i++) {
+        if (auto[i].benzinesE == true) {
+            benzinesDarabSzam++;
+        }
+    }
+    return benzinesDarabSzam;
+}
+var a1 = {
+    gyarto: "Opel",
+    tipus: "Astra",
+    hengerurtartalom: 1600,
+    benzinesE: true
+};
+var a3 = {
+    gyarto: "Bmw",
+    tipus: "I3",
+    hengerurtartalom: 0,
+    benzinesE: false
+};
+var a2 = {
+    gyarto: "Volkswagen",
+    tipus: "Passat",
+    hengerurtartalom: 2200,
+    benzinesE: false
+};
+var a4 = {
+    gyarto: "Ford",
+    tipus: "Mondeo",
+    hengerurtartalom: 1600,
+    benzinesE: true
+};
+
+var autok = [a1, a2, a3, a4];
--- feladat1.ts
+++ feladat1.ts
@@ -8,7 +8,7 @@ function negativE(szam:number[]):boolean{
         }
     }
     return true;
-}
+};
 
 function korKerTer(r:number):[number,number]{
 
@@ -16,14 +16,14 @@ function korKerTer(r:number):[number,number]{
     let terulet: number = r * r *  Math.PI;
 
     return [kerulet, terulet];
-}
+};
 
 interface Auto {
-    Gyarto: string,
-    Tipus: string,
-    Hengerurtartalom: number,
-    BenzinesE: boolean
-}
+    gyarto: string,
+    tipus: string,
+    hengerurtartalom: number,
+    benzinesE: boolean
+};
 
 function minAuto(auto:Auto[]):Auto{
 
@@ -35,14 +35,78 @@ function minAuto(auto:Auto[]):Auto{
         }
     }
     return minCCM;
-}
+};
 
 function benzinesDB(auto:Auto[]):number{
     let benzinesDarabSzam = 0;
     for(let i: number = 0; i < auto.length; i++){
-        if(auto[i].BenzinesE == true){
+        if(auto[i].benzinesE == true){
             benzinesDarabSzam++;
         }
     }
     return benzinesDarabSzam;
+};
+
+let a1:Auto = {
+    gyarto:"Opel",
+    tipus:"Astra",
+    hengerurtartalom: 1600,
+    benzinesE:true
+};
+
+let a3:Auto = {
+    gyarto:"Bmw",
+    tipus:"I3",
+    hengerurtartalom:0,
+    benzinesE:false
+};
+
+let a2:Auto = {
+    gyarto:"Volkswagen",
+    tipus:"Passat",
+    hengerurtartalom: 2200,
+    benzinesE:false
+};
+
+let a4:Auto = {
+    gyarto:"Ford",
+    tipus:"Mondeo",
+    hengerurtartalom: 1600,
+    benzinesE:true
+};
+
+let autok: Auto = [a1,a2,a3,a4];
+
+function atlagCCM(autok:Auto[]):number{
+
+    let atlag: number = 0;
+
+    for(let i = 0; i < autok.length; i++){
+        atlag += autok[i].hengerurtartalom;
+        atlag /= autok.length;
+    }
+    return atlag;
+};
+
+function vanENemBenzines(autok:Auto[]):boolean{
+    let i: number = 0;
+    while(i < autok.length && !(autok[i].benzinesE == false)){
+        i++;
+    }
+    return i < autok.length;
+}
+
+function BenzinesekSzetvalogatasa(autok:Auto[]):[Auto[],Auto[]]{
+    let benzinesek: Auto [] = [];
+    let nemBenzinesek: Auto [] = [];
+    for(let i: number = 0; i < autok.length; i++){
+        if(autok[i].benzinesE == true){
+            benzinesek.push(autok[i]);
+        }
+        else{
+            nemBenzinesek.push(autok[i]);
+        }
+    } 
+
+    return [benzinesek,nemBenzinesek];
 }
